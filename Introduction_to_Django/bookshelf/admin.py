c16d3028d0b528797c925ae37071f30b89930588
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-19 02:17:26 +0300

    addmin confilict fixed

--- Introduction_to_Django/bookshelf/admin.py
+++ Introduction_to_Django/bookshelf/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .models import Book
 
 @admin.register(Book)
@@ -11,19 +9,3 @@ class BookAdmin(admin.ModelAdmin):
 
 # Alternative registration without decorator:
 # admin.site.register(Book, BookAdmin)
-=======
-
-# Register your models here.
->>>>>>> 08a2d9d (Add bookshelf app with Book model and documented CRUD operations)
-=======
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
-    list_filter = ('publication_year',)  # Filters on the right sidebar
-    search_fields = ('title', 'author')  # Search box for title and author
-
-# Alternative registration without decorator:
-# admin.site.register(Book, BookAdmin)
->>>>>>> 09b052c (admin.py and admin-setup created)
