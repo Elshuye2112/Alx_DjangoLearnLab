f7fbc98883bb9e9eb38434c71c0f9ca58c6d10a9
Elshaday Basie Kassa <basie.elsh2112@gmail.com>
2025-07-18 23:51:42 +0300

    view page created

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
@@ -25,14 +26,27 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     else:
         instance.userprofile.save()
 =======
+=======
+from django.contrib.auth.models import User
+>>>>>>> 178126a (view page created)
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.role}"
 
+<<<<<<< HEAD
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -54,3 +68,12 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 >>>>>>> fea6b56 (django modeles created)
+=======
+# Signal to create or update user profile
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
+>>>>>>> 178126a (view page created)
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('admin-role/', views.admin_view, name='admin_view'),
     path('librarian-role/', views.librarian_view, name='librarian_view'),
     path('member-role/', views.member_view, name='member_view'),
@@ -12,4 +13,9 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
 >>>>>>> f4cc49a (authentication added)
+=======
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+>>>>>>> 178126a (view page created)
 ]
