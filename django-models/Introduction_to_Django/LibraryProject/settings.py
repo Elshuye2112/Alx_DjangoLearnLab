c81157ae74aa501cf566550416ea32d58b35c821
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-19 01:51:33 +0300

    ddd

--- django-models/Introduction_to_Django/LibraryProject/settings.py
+++ django-models/Introduction_to_Django/LibraryProject/settings.py
@@ -34,8 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'bookshelf',
     'django.contrib.staticfiles',
 ]
