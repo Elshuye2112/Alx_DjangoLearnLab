0cc4a4f553100e0f5e621a48696cbdccd31321b6
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 00:59:18 +0300

    authentication view url added

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,11 +1,12 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
+    # ✅ Authentication views expected in this file
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
