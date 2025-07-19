5026bc07972a5abe738fb762d307e84363181fc4
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 02:39:24 +0300

    unecessary import  is deleted

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.views.generic.detail import DetailView
 from .models import Library, Book
-from .forms import BookForm  # make sure this exists
 from django.contrib.auth.decorators import permission_required
 # Role check functions
 def is_admin(user):
