e1068f82f81d34911575d4a7b0e358d1a478b58d
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 01:08:44 +0300

    authentication view url added

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # ✅ Authentication views expected in this file
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),
 ]
---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register, list_books, LibraryDetailView
+   
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+]
