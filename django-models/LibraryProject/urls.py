9823c5c37c8df3969f511433a0a75462f707f816
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 00:56:22 +0300

    authentication view url added

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),
+
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
