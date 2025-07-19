f5dc9a6089b8cd72ecdbd0b6924aeaffbe026eb0
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 00:42:17 +0300

    view and url are edit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
 ]
+
+
+    
