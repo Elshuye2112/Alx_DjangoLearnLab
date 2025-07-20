b727b62870af590953788a6753df72e3ede2ca92
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 04:07:26 +0300

    directory rearangement has been done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from . import views
+from .views import list_books, add_book, edit_book, delete_book, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/', list_books, name='list_books'),
+    path('books/add/',add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 
-    path('add_book/', views.add_book),       # for test script
-    path('edit_book/<int:pk>/', views.edit_book),  # for test script
+    path('add_book/', add_book),       # for test script
+    path('edit_book/<int:pk>/', edit_book),  # for test script
 
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register, name='register'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
