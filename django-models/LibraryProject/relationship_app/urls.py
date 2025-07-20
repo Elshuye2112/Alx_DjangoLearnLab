a1e90e560480cf6534dbb56df41ccba69478070e
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 04:11:54 +0300

    url is rearanged

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('edit_book/<int:pk>/', edit_book),  # for test script
 
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
+    path('register/', register, name='views.register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
