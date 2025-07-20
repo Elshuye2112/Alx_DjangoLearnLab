68f56a5a007bb08f2551e7eb3c609b96a05acccd
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-20 03:44:27 +0300

    directory rearangement has been done

--- django-models/LibraryProject/LibraryProject/relationship_app/forms.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/forms.py
@@ -1,8 +1,7 @@
-# relationship_app/forms.py
 from django import forms
 from .models import Book
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author']  # adjust fields as per your Book model
+        fields = ['title', 'author', 'published_date']
