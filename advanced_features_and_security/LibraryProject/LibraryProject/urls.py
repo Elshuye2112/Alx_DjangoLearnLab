e1e0b871135e32abfc78bc1e4ef094486a278c77
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 02:11:55 +0300

    form.py is created in bookshelf

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
     path('relationship_app/', include('relationship_app.urls')),  # This is key
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('', views.book_list, name='book_list'),
+    path('create/', views.book_create, name='book_create'),
+    path('edit/<int:pk>/', views.book_edit, name='book_edit'),
+    path('delete/<int:pk>/', views.book_delete, name='book_delete'),
 ]
