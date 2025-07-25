7774cc7d46a903564fb12fcc06a67372ffd98e07
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 02:53:38 +0300

    https or secure cookies added

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,18 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']
+class ExampleForm(forms.Form):
+    title = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Book Title'})
+    )
+    author = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Author Name'})
+    )
+    published_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={'type': 'date'})
+    )
