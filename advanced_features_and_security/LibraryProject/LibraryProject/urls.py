5009fb89a81c50fce7c8c26b625945f1c6ac393f
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 02:41:40 +0300

    security policy added

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
 from django.urls import path
-from . import views
+from bookshelf import views as bookshelf_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', lambda request: redirect('admin/')),
@@ -11,8 +12,12 @@ urlpatterns = [
     path('relationship_app/', include('relationship_app.urls')),  # This is key
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('', views.book_list, name='book_list'),
-    path('create/', views.book_create, name='book_create'),
-    path('edit/<int:pk>/', views.book_edit, name='book_edit'),
-    path('delete/<int:pk>/', views.book_delete, name='book_delete'),
+
+    # Use bookshelf_views here:
+    path('', bookshelf_views.book_list, name='book_list'),
+    path('create/', bookshelf_views.book_create, name='book_create'),
+    path('edit/<int:pk>/', bookshelf_views.book_edit, name='book_edit'),
+    path('delete/<int:pk>/', bookshelf_views.book_delete, name='book_delete'),
+
+    path('', RedirectView.as_view(url='/relationship_app/books/', permanent=False)),
 ]
