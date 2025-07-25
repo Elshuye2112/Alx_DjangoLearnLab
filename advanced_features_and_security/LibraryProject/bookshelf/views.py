aafe4257e4ce9ce59e3d0efa3f2f079b5f51a5a3
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 02:58:51 +0300

    example form is created

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,19 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from .forms import BookForm
-
+from .forms import ExampleForm
+from django.contrib.auth.decorators import login_required
+@login_required
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data safely here
+            # e.g., save or use cleaned_data
+            return redirect('some_success_url')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
