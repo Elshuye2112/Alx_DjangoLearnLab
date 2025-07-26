fb726b0e34abd05c003f9f6c25bbb7e47f93fc03
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 23:15:04 +0300

    custom-user changed to user.model

--- advanced_features_and_security/LibraryProject/users/models.py
+++ advanced_features_and_security/LibraryProject/users/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
