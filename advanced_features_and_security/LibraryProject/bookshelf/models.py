eb71327e86727c61c0b888cf8428d24bdae50bd1
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 23:55:52 +0300

    admin added to the model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ["username", "email", "first_name", "last_name", "is_staff", "date_of_birth"]
+    search_fields = ["username", "email", "first_name", "last_name"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
