e04f45d39d0d5c7f95f5da4124a3ae2823c23806
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-07-26 23:44:50 +0300

    custom-user changed to user.model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
@@ -7,6 +7,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, email=None, password=None, date_of_birth=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
