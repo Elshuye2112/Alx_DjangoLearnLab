8e917fb769329034e534007692b2f353587e5f2d
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-08-11 19:58:56 +0300

    route modification

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet,BookCreateView,BookDeleteView,BookUpdateView,BookDetailView,BookListView
-
-router = DefaultRouter()
-router.register(r'authors', AuthorViewSet)
-router.register(r'books', BookViewSet)
+from django.urls import path
+from .views import (
+    AuthorViewSet,
+    BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('authors/', AuthorViewSet.as_view({'get': 'list', 'post': 'create'}), name='author-list'),
+    path('authors/<int:pk>/', AuthorViewSet.as_view({'get': 'retrieve'}), name='author-detail'),
+
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
