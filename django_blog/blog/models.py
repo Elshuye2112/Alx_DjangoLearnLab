730a56dfde7d484d4ec3486669075c5934eb39be
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-08-17 02:05:35 +0300

    tag created

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -12,6 +12,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
