c0d99c666aad99ea587cfcf17420c018f3bf387b
Elshaday Basie <93201203+Elshuye2112@users.noreply.github.com>
2025-08-17 01:49:51 +0300

    route for the crud is created in blog/url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
 # blog/urls.py
 from django.urls import path
-from .views import PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView, add_comment, delete_comment, edit_comment, register_view, login_view, logout_view, profile_view
+from .views import CommentCreateView, CommentDeleteView, CommentUpdateView, PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView, add_comment, delete_comment, edit_comment, register_view, login_view, logout_view, profile_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -15,4 +15,7 @@ urlpatterns = [
     path('posts/<int:post_id>/comments/new/', add_comment, name='add-comment'),
     path('comments/<int:comment_id>/edit/', edit_comment, name='edit-comment'),
     path('comments/<int:comment_id>/delete/', delete_comment, name='delete-comment'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
